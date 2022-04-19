6232e0e88a449f180ae901f23e596c292133b695
crisepu <csepulvedavenegas@gmail.com>
2022-04-19 15:53:02 +0000

    first advance

--- src/js/component/selectPlayer.jsx
+++ src/js/component/selectPlayer.jsx
@@ -6,7 +6,20 @@ const SelectPlayer = () => {
 
 	return (
 		<>
-			<h2 className="title2">Pick a Weapon</h2>;
+			<h2 className="title2">Pick a Weapon</h2>
+			<div className="startingDiv">
+				<h3 className="title3">CHOOSE YOUR WEAPON</h3>
+				<input type="text" placeholder="Player 1 username" />
+				<input type="text" placeholder="Player 2 username" />
+				<div>
+					<button className="icon">
+						<h1 className="iconX">X</h1>
+					</button>
+					<button className="icon">
+						<h1 className="iconO">O</h1>
+					</button>
+				</div>
+			</div>
 		</>
 	);
 };
