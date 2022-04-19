960452cfbc44ae602864e261b65461ac89d047ce
crisepu <csepulvedavenegas@gmail.com>
2022-04-19 18:21:19 +0000

    advance exercise

---
+++ src/js/component/game.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//create your first component
+const Game = () => {
+	return (
+		<>
+			<h1 className="title1">Game de prueba</h1>;
+		</>
+	);
+};
+
+export default Game;
--- src/js/component/selectPlayer.jsx
+++ src/js/component/selectPlayer.jsx
@@ -1,27 +1,64 @@
 import React, { useState } from "react";
+import Game from "./game.jsx";
 
 //create your first component
 const SelectPlayer = () => {
-	const [player, setPlayer] = useState("");
+	const [player1, setPlayer1] = useState("");
+	const [player2, setPlayer2] = useState("");
+	const [playerStart, setPlayerStart] = useState("");
 
-	return (
-		<>
-			<h2 className="title2">Pick a Weapon</h2>
-			<div className="startingDiv">
-				<h3 className="title3">CHOOSE YOUR WEAPON</h3>
-				<input type="text" placeholder="Player 1 username" />
-				<input type="text" placeholder="Player 2 username" />
-				<div>
-					<button className="icon">
-						<h1 className="iconX">X</h1>
-					</button>
-					<button className="icon">
-						<h1 className="iconO">O</h1>
-					</button>
+	function setPlayers(player1, player2, playerStart) {
+		setPlayer1(player1);
+		setPlayer2(player2);
+		setPlayerStart(playerStart);
+	}
+
+	if (player1 === "" || player2 === "") {
+		return (
+			<>
+				<h2 className="title2">Pick a Weapon</h2>
+				<div className="startingDiv">
+					<h3 className="title3">CHOOSE YOUR WEAPON</h3>
+					<input
+						id="inputPlayer1"
+						type="text"
+						placeholder="Player 1 username"
+					/>
+					<input
+						id="inputPlayer2"
+						type="text"
+						placeholder="Player 2 username"
+					/>
+					<div>
+						<button
+							className="icon"
+							onClick={() =>
+								setPlayers(
+									inputPlayer1.value,
+									inputPlayer2.value,
+									"player1"
+								)
+							}>
+							<h1 className="iconX">X</h1>
+						</button>
+						<button
+							className="icon"
+							onClick={() =>
+								setPlayers(
+									inputPlayer1.value,
+									inputPlayer2.value,
+									"player2"
+								)
+							}>
+							<h1 className="iconO">O</h1>
+						</button>
+					</div>
 				</div>
-			</div>
-		</>
-	);
+			</>
+		);
+	} else {
+		return <Game />;
+	}
 };
 
 export default SelectPlayer;
